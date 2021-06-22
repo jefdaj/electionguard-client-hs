ed2e6a7df7db7e9d7383fb90ba4a81c4441aebd3
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-06-22 08:18:08 -0700

    draft Guardian ping

--- src/ElectionGuard/API/Guardian.hs
+++ src/ElectionGuard/API/Guardian.hs
@@ -1,3 +1,17 @@
+{-# LANGUAGE ExplicitForAll #-}
+
 module ElectionGuard.API.Guardian
-  ()
+  ( ping, PingResponse
+  )
   where
+
+import Network.HTTP.Client (HttpException, Response)
+import ElectionGuard.API.Guardian.Generated.Common (Configuration, SecurityScheme, MonadHTTP)
+import ElectionGuard.API.Guardian.Generated.Types.HTTPValidationError
+import ElectionGuard.API.Guardian.Generated.Operations.PingApiV1PingGet
+
+-- TODO does this make sense? maybe go with ByteString first
+type PingResponse = Response PingApiV1PingGetResponse
+
+ping :: forall m s . (MonadHTTP m, SecurityScheme s) => Configuration s -> m (Either HttpException PingResponse)
+ping = pingApiV1PingGet
