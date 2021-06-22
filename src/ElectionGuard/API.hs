0c95a572276259794844f14cb6a8b7e9f31a6292
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-06-22 15:17:41 -0700

    add ElectionGuard.API module

---
+++ src/ElectionGuard/API.hs
@@ -0,0 +1,32 @@
+module ElectionGuard.API
+
+  -- Generated.Common
+  ( Configuration (..)
+  , doCallWithConfiguration
+  , doCallWithConfigurationM
+  , doBodyCallWithConfiguration
+  , doBodyCallWithConfigurationM
+  , runWithConfiguration
+  , MonadHTTP (..)
+  , stringifyModel
+  , StringifyModel
+  , SecurityScheme (..)
+  , AnonymousSecurityScheme (..)
+  , textToByte
+  , JsonByteString (..)
+  , JsonDateTime (..)
+  , RequestBodyEncoding (..)
+
+  -- Generated.Configuration
+  , defaultURL
+  , defaultConfiguration
+
+  -- Generated.Types
+  , HTTPValidationError
+
+  )
+  where
+
+import ElectionGuard.API.Generated.Common
+import ElectionGuard.API.Generated.Configuration
+import ElectionGuard.API.Generated.Types
